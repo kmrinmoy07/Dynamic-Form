7c886813a9d611e8f9ecec302616d9c9486cf6aa
Mrinmoy Kumar <mrinmoykumar20@gmail.com>
2026-06-12 19:10:10 +0530

    Initial Commit

---
+++ DynamicForm/DynamicFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormApp.swift
+//  DynamicForm
+//
+//  Created by Mrinmoy Kumar on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
